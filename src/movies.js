13187e88404f251f464100850067ad711d3844ff
JorgeAngst <jorge.rodriguez.web@gmail.com>
2020-04-22 20:33:16 +0200

    almost done

--- src/movies.js
+++ src/movies.js
@@ -20,26 +20,32 @@ function howManyMovies(movies) {
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(movies) {
-  const rate = movies.reduce((sum, movie) => {
-   return sum + movie.rate;
-  }, 0);
-  return rate/movies.length
+
+  if (movies.length === 0) {
+    return 0;
+  } else {
+    const rate = movies.reduce((sum, movie) => {
+        return (sum + movie.rate);
+    }, 0);
+    const avgRate = (rate / movies.length).toFixed(2)
+    return parseFloat(avgRate)
+  }
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movies) {
+  
   const dramaMovies = movies.filter(movie => movie.genre.includes("Drama"))
-  const avgRate = dramaMovies.reduce((acc, movie) => {
-    return (acc + movie.rate);
-  }, 0)
-  return avgRate / dramaMovies.length
+  
+  return ratesAverage(dramaMovies)
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movies) {
-
+  
   const moviesByYear = [...movies]
 
+  
   moviesByYear.sort((a, b) => {
     if (a.year > b.year) {
       return 1;
@@ -49,14 +55,35 @@ function orderByYear(movies) {
       return a.title.localeCompare(b.title)
     }
   })
-
+  
   return moviesByYear
 }
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(movies) {
-  
+
+  const titles = movies.map(movie => movie.title) 
+
+  titles.sort()
+
+  if (titles.length < 20) {
+    return titles;
+  } else {
+    return titles.splice(0,20)
+  }
 }
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies) {
+  const duration = movies.filter(movie => 
+    movie.duration = parseInt(movie.duration.slice(0, 1) * 60) + parseInt(movie.duration.slice(2, -3) == '' ? ' 0' : movie.duration.slice(2, -3))
+  ) 
+  return duration  
+} 
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(movies) {
+  return orderByYear(movies)
+} 
+
+console.log(bestYearAvg(movies))
